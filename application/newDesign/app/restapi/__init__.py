0c6f075d4c37bf7af264913c71eda829876605e2
Axel Gard <axel.gard@protonmail.com>
2019-10-14 21:10:22 +0200

    rm flask templa gowing back to prev templa

--- application/newDesign/app/restapi/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- encoding: utf-8 -*-
-"""
-Argon Dashboard - coded in Flask
-
-Author  : AppSeed App Generator
-Design  : Creative-Tim.com
-License : MIT 
-Support : https://appseed.us/support 
-"""
